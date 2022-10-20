8b238a3e57c8a76f6f338431ab1a7976fd8e1847
jekk18 <chelidzejeko9123741jeko@gmail.com>
2022-10-20 17:32:51 +0400

    fixes for deployment

--- src/pages/Radio/Radio.js
+++ src/pages/Radio/Radio.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./Radio.css";
-import video from '../../assets/img/video.mp4'; 
+// import video from '../../assets/img/video.mp4'; 
 
 const Radio = () => {
   return (
      <div className="video-box">
          <video autoPlay muted loop id="myVideo" >
-            <source src={video} type="video/mp4"></source>
+            {/* <source src={video} type="video/mp4"></source> */}
         </video> 
         <h1>Radio.</h1> 
      </div>
